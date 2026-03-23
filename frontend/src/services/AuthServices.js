b0ba15b7f736b4bfc36cfff2f5cac018ea93ade7
Rudrasharma214 <rudrashma.810@gmail.com>
2026-03-23 23:12:17 +0530

    Add AuthContext, AuthServices, and placeholder hooks for useMutation and useQueries

---
+++ frontend/src/services/AuthServices.js
@@ -0,0 +1,11 @@
+import api from "./ApiInstance"
+
+export const login = async (email,password) => {};
+
+export const signup = async (name,email,password) => {};
+
+export const changePassword = async (currentPassword,newPassword) => {};
+
+export const logout = async () => {};
+
+export const refreshToken = async () => {};
